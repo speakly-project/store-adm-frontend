b39cc7cf9939774106afc97dbf2746a61f84853d
Alberto Rubio F.dez <alberto13101@gmail.com>
2025-12-01 09:23:06 +0100

    Create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { PAdmin } from './pages/p-admin/p-admin';
-import { PHome } from './pages/p-home/p-home';
-import { PLogin } from './pages/p-login/p-login';
+
 
 export const routes: Routes = [
-    {path: '', component: PHome},
-    {path: 'login', component: PLogin},
-    {path: 'admin', component: PAdmin},
+    {path: '', component: Home},
+    {path: 'login', component: Login},
+    {path: 'cursos', component: Cursos},
+    {path: 'cursos/:id', component: Cursos},
+    {path: 'categorias', component: Categorias},
     {path: 'error', component: Error},
     {path: '**', redirectTo: ''}, 
 ];
