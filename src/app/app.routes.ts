a72ebefbb2e92300635793c6686c7a0f7c0e79d8
Alberto Rubio F.dez <alberto13101@gmail.com>
2025-12-02 11:48:01 +0100

    Revert "Revert "feat(speakly-project/sprints#2): Configuración de rutas""

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'login', component: Login},
+    {path: 'cursos', component: Cursos},
+    {path: 'cursos/:id', component: Cursos},
+    {path: 'categorias', component: Categorias},
+    {path: 'error', component: Error},
+    {path: '**', redirectTo: ''}, 
+];
