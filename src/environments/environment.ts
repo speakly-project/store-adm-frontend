c914925d6724cb7d1233ae09729936720df814e1
EnriqueMislata <soyenriqueh@hotmail.com>
2026-01-26 11:37:12 +0100

    fix: url, otra vez

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://speakly-bank-back.preproducciondaw.cip.fpmislata.com/'
+  apiUrl: 'http://speakly-store-back.preproducciondaw.cip.fpmislata.com/'
 };
